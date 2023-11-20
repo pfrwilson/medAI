b844089729a91c3cdc7fdc7cc4fae5e3df7401b0
MahdiGilany_Vector <mahdi.gilany@queensu.ca>
2023-11-20 14:52:12 -0500

    debugging

--- projects/tta/utils/metrics.py
+++ projects/tta/utils/metrics.py
@@ -1,12 +1,17 @@
 from sklearn.metrics import roc_auc_score, balanced_accuracy_score
 from typing import Dict, List, Tuple
 import numpy as np
+import torchmetrics
 
 
 class MetricCalculator(object):
-    list_of_metrics: List[str] = [
-        roc_auc_score,
-        balanced_accuracy_score,
+    # list_of_metrics: List[str] = [
+    #     roc_auc_score,
+    #     balanced_accuracy_score,
+    # ]
+    list_of_metrics: List = [
+        torchmetrics.functional.auroc,
+        torchmetrics.functional.accuracy,
     ]
     
     def __init__(self, avg_core_logits_first: bool = False):
@@ -20,61 +25,64 @@ class MetricCalculator(object):
         self.core_id_labels = {}
 
     def update(self, batch_meta_data, logits, labels):
-        for id_tensor in batch_meta_data["id"]:
+        for i, id_tensor in enumerate(batch_meta_data["id"]):
             id = id_tensor.item()
             
             if id in self.core_id_logits:
-                self.core_id_logits[id].append(logits)
-                self.core_id_labels[id].append(labels)
+                self.core_id_logits[id].append(logits[i])
+                self.core_id_labels[id].append(labels[i])
             else:
-                self.core_id_logits[id] = [logits]
-                self.core_id_labels[id] = [labels]
+                self.core_id_logits[id] = [logits[i]]
+                self.core_id_labels[id] = [labels[i]]
 
     def get_metrics(self):
         patch_metrics: Dict = self.get_patch_metrics()
         core_metrics: Dict = self.get_core_metrics()
-        return patch_metrics.update(core_metrics)
+        patch_metrics.update(core_metrics)
+        return patch_metrics
     
     def get_patch_metrics(self):
         logits = np.concatenate(
-            [np.concatenate(logits_list) for logits_list in self.core_id_logits.values()]
+            [np.asarray(logits_list) for logits_list in self.core_id_logits.values()]
             )
         labels = np.concatenate(
-            [np.concatenate(labels_list) for labels_list in self.core_id_labels.values()]
+            [np.asarray(labels_list) for labels_list in self.core_id_labels.values()]
             )
-        return self._get_metrics(logits, labels, prefix="patch_")
+        return self._get_metrics(logits, logits.argmax(axis=1), labels, prefix="patch_")
     
     def get_core_metrics(self):
         
         if self.avg_core_logits_first:
             logits = np.concatenate(
                 [np.mean(
-                    np.concatenate(logits_list),
+                    np.asarray(logits_list),
                     axis=0
                     ) for logits_list in self.core_id_logits.values()]
                 )          
+            predicted_labels = logits.argmax(axis=1)
         else:
-            logits = np.asarray(
+            logits = np.array(
                 [np.mean(
-                    np.argmax(np.concatenate(logits_list), axis=1),
+                    np.argmax(np.asarray(logits_list), axis=1),
                     axis=0
                     ) for logits_list in self.core_id_logits.values()]
                 )
+            predicted_labels = logits >= 0.5
 
         labels = np.asarray(
-            [labels_list[0][0] for labels_list in self.core_id_labels.values()]
+            [labels_list[0] for labels_list in self.core_id_labels.values()]
             )
             
-        return self._get_metrics(logits, labels, prefix="core_")
+        return self._get_metrics(logits, predicted_labels, labels, prefix="core_")
     
-    def _get_metrics(self, logits, labels, prefix=""):
+    def _get_metrics(self, logits, predicted_labels, labels, prefix=""):
         metrics: Dict = {}
         for metric in self.list_of_metrics:
             metric_name: str = metric.__name__
             try:
-                metric_value: float = metric(labels, logits)
+                metric_value: float = metric(logits, labels, task="binary")
             except:
-                metric_value: float = metric(labels, logits.argmax(axis=1))
+                metric_value: float = metric(predicted_labels, labels, task="binary", num_classes=2)
             metrics[prefix + metric_name] = metric_value
         return metrics
 
