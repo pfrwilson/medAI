9c89154abf1ecdfc6c9ccae8f7e9f59ec3cd694d
Paul Wilson <ypfrwilson@gmail.com>
2024-01-27 13:56:35 -0500

    add job submission scripts

---
+++ projects/miccai2024/.slurm/run_ssl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:a40:1
+#SBATCH --time 8:00:00
+#SBATCH -c 16 
+#SBATCH --qos=m2
+#SBATCH --output=slurm-%j.log
+
+# send this batch script a SIGUSR1 60 seconds
+# before we hit our time limit
+#SBATCH --signal=B:USR1@60
+
+export TQDM_MININTERVAL=30
+
+python train_medsam_ssl.py 
