9f7419ef052699ff3bd40cf54de116a092b34ff8
Paul Wilson <ypfrwilson@gmail.com>
2024-02-23 16:22:05 -0500

    fix dependency

--- medAI/modeling/medsam/segment_anything/predictor.py
+++ medAI/modeling/medsam/segment_anything/predictor.py
@@ -5,13 +5,12 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Optional, Tuple
+
 import numpy as np
 import torch
 
-from segment_anything.modeling import Sam
-
-from typing import Optional, Tuple
-
+from .modeling import Sam
 from .utils.transforms import ResizeLongestSide
 
 
