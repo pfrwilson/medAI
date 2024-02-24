9a2f22b17d3eef67766dd45e0d64dac989f63151
Paul Wilson <ypfrwilson@gmail.com>
2024-02-24 16:47:11 -0500

    add hdf5 data accessor

--- medAI/datasets/nct2013/data_access.py
+++ medAI/datasets/nct2013/data_access.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import skimage.transform
 from PIL import Image
+from tqdm import tqdm
 
 
 class ExactNCT2013DataAccessorBase:
@@ -104,6 +105,50 @@ class VectorClusterExactNCT2013DataAccessor(ExactNCT2013DataAccessorBase):
             self._h5_file.close()
 
 
+class SmallFormatHDF5ExactNCT2013DataAccessor(ExactNCT2013DataAccessorBase):
+    def __init__(self, path):
+        self.h5_file = h5py.File(path, "r")
+        self.metadata_path = path.replace(".h5", "_metadata.csv")
+
+        if not os.path.exists(self.metadata_path):
+            self._create_metadata()
+        else:
+            self._load_metadata()
+
+    def get_metadata_table(self):
+        return self.metadata
+
+    def get_rf_image(self, core_id, frame_idx=0):
+        assert frame_idx == 0, "Only frame_idx=0 is supported"
+        return self.h5_file[core_id]["rf"]
+
+    def get_bmode_image(self, core_id, frame_idx=0):
+        assert frame_idx == 0, "Only frame_idx=0 is supported"
+        return self.h5_file[core_id]["bmode"]
+
+    def get_num_frames(self, core_id):
+        return 1  # Only 1 frame per core_id
+
+    def get_prostate_mask(self, core_id):
+        return self.h5_file[core_id]["prostate_mask"]
+
+    def get_needle_mask(self, core_id):
+        return self.h5_file[core_id]["needle_mask"]
+
+    def _create_metadata(self):
+        metadata = []
+        for core_id in tqdm(self.h5_file.keys()):
+            metadata.append(dict(self.h5_file[core_id].attrs))
+        self.metadata = pd.DataFrame(metadata)
+        self.metadata.to_csv(self.metadata_path, index=False)
+
+    def _load_metadata(self):
+        self.metadata = pd.read_csv(self.metadata_path)
+
+    def __del__(self):
+        self.h5_file.close()
+
+
 def setup_data_accessor():
     """Sets up the data accessor for the NCT2013 dataset.
 
@@ -111,9 +156,18 @@ def setup_data_accessor():
     data accessor. class instance
     """
 
-    return VectorClusterExactNCT2013DataAccessor(
-        prostate_segmentation_dir="/ssd005/projects/exactvu_pca/nct_segmentations_medsam_finetuned_2023-11-10"
-    )
+    if os.path.exists("/ssd005/projects/exactvu_pca/nct2013"):
+        # we are on the vector server
+        return VectorClusterExactNCT2013DataAccessor(
+            prostate_segmentation_dir="/ssd005/projects/exactvu_pca/nct_segmentations_medsam_finetuned_2023-11-10"
+        )
+    else:
+        if "NCT2013_HDF5_PATH" in os.environ:
+            return SmallFormatHDF5ExactNCT2013DataAccessor(
+                os.environ["NCT2013_HDF5_PATH"]
+            )
+        else:
+            raise ValueError("NCT2013_HDF5_PATH environment variable not set")
 
 
 data_accessor = setup_data_accessor()
